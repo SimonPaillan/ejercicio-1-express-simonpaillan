44aae1eed5cc4496a1341bb9958a21e6df82d8c9
simon <simon249@hotmail.com>
2026-04-04 20:14:38 -0300

    finalizado ej2

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use( express.static('public'));
 
 // Ruta raíz
 app.get('/', (req, res) => {
-  res.send('Hola mundo!');
+  res.send('Hola mundo nashe!');
 });
 
 // Middleware básico para manejo de errores
@@ -16,12 +17,10 @@ app.use((err, req, res, next) => {
   res.status(500).json({ error: '¡Algo salió mal!' });
 });
 
+
 // Iniciar servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor ejecutándose en el puerto ${PORT}`);
 }); 
 
-// Iniciar el express
-express.static(root, [options])
-app.use('/static', express.static('public'));
