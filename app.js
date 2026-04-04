5eca617af3468cf096ef41b73fe215f3707343f5
simon <simon249@hotmail.com>
2026-04-04 15:39:25 -0300

    a

--- app.js
+++ app.js
@@ -21,3 +21,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor ejecutándose en el puerto ${PORT}`);
 }); 
+
+// Iniciar el express
+express.static(root, [options])
+app.use('/static', express.static('public'));
